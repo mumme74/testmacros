7a809f3569719ab94f9bf35db0738c6a9ca14554
Fredrik Johansson <fredrik@mumme.se>
2019-04-20 22:00:45 +0200

    Added delay funtion, where cpu can do other tasks

--- testmacros.h
+++ testmacros.h
@@ -4,6 +4,7 @@
 // shuld be placed in top of file outside of a function
 #define initTests() \
 	unsigned int _errCnt = 0, _testsCnt = 0; \
+	bool _delayed = false; \
 	template <typename T> \
 	void testWT(const char *exprStr, const char *expectStr, T expr, T expect, int line) { \
 		T e =(expr); T ex = (expect); ++_testsCnt;  \
@@ -15,6 +16,16 @@
 			Serial.print("passed ");Serial.print(exprStr);Serial.print(" == ");Serial.print(expectStr);Serial.print(" at line: "); Serial.println(line);Serial.flush();\
 		} \
 	} \
+	void testDelay(unsigned long ms) { \
+		_delayed = true; \
+		unsigned long endAt = millis() + ms; \
+		do { \
+			yield(); \
+			loop(); \
+		} while (endAt >= millis()); \
+		_delayed = false; \
+	}
+	
 
 // auto detect types to compare with
 #define test(expr, expect) { typedef decltype(expect) _type; testWT<_type>(#expr, #expect, expr, expect, __LINE__); }
@@ -28,8 +39,9 @@
 									Serial.print(_errCnt);Serial.println(" errors");
 									
 // insert i loop() listens to input in Serial.in and runs tests when it has any input
-#define testLoop() 	if (Serial.available() > 0) { \
-    Serial.read(); runTests(); \
+#define testLoop() 	if (Serial.available() > 0 && !_delayed) { \
+    Serial.read(); \
+    runTests(); \
   } 
 
 #endif // TESTMACROS_H
